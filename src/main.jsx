17810b0db2e48ecd45661531b2096a4182a9f613
Arishusman <arishusm12an@gmail.com>
2026-08-14 17:53:24 +0500

    Update website design and changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "../responsive.css";
+import "./responsive.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
